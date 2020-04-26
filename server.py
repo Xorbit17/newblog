4e50069a18de26767a740fd4e7772ba3435d4570
dv <yamaHA!MT-09>
2020-04-26 21:13:51 +0200

    Beetje klooien ifv les voor pullen van DJ content

--- server.py
+++ server.py
@@ -3,24 +3,38 @@ import tornado.ioloop as ioloop
 import os
 import hashlib
 import database  # Hier wordt code uitgevoerd!!
+import re
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 STATIC_FILE_DIR = os.path.join(CURRENT_PATH, "static")
 
+USER_REGEX = r"^(?=.{2,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$"
+PASS_REGEX = r"^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$"
+
+user_regex_compiled = re.compile(USER_REGEX)
+pass_regex_compiled = re.compile(PASS_REGEX)
 
 class MainHandler(web.RequestHandler):
+    """This wil process templates"""
     def get(self, arg1):
         # user = database.get_user_by_id(user_id)
-        self.write("Het werkt: {}. Met user. {}".format(arg1,self.cookies.get("user_id")))
-
-
+        self.write("Het werkt: {}. Met user. {}".format(arg1, self.cookies.get("user_id")))
 
 
 class LoginHandler(web.RequestHandler):
-
+    """Handles login form request with a body"""
     def post(self):
         user_name = self.get_body_argument("user-name")
         password = self.get_body_argument("password")
+        # Bevatten user name en paswoord geen stoute dingen?
+        if user_regex_compiled.match(user_name) is None:
+            self.redirect("/static/login.html")
+            return
+        if pass_regex_compiled.match(password) is None:
+            # Nu zijn de rapen gaar
+            pass
+            return
+
         hasher = hashlib.sha256()
         hasher.update(password.encode("utf-8"))
         # Add illegal char check?
@@ -41,10 +55,11 @@ class LoginHandler(web.RequestHandler):
 
             self.redirect("/home.html")
         else:
-            #self.redirect("/static/password-not-correct.html")
+            # self.redirect("/static/password-not-correct.html")
             self.set_cookie("user_id", str(check_user.id))
             self.redirect("/home.html")
             return
+
 
 class NewUserHandler(web.RequestHandler):
 
@@ -66,7 +81,6 @@ class NewUserHandler(web.RequestHandler):
         new_user.user_name = user_name
         new_user.save_to_db()
         self.redirect("/static/login.html")
-
 
 
 def make_tornado_app():
