efaf89c159d368ff180ec7db579dc5121add71a5
Djamilla <djamilla.simoens@gmail.com>
2020-05-27 20:04:27 +0200

    lastest update

--- server.py
+++ server.py
@@ -5,9 +5,12 @@ import hashlib
 import database  # Hier wordt code uitgevoerd!!
 import re
 
+import model
+import util
+
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 STATIC_FILE_DIR = os.path.join(CURRENT_PATH, "static")
-TEMPLATE = os.path.join(CURRENT_PATH, "templates") #nog eens opzoeken!!!
+TEMPLATE = os.path.join(CURRENT_PATH, "templates")
 
 USER_REGEX = r"^(?=.{2,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$"
 PASS_REGEX = r"^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$"
@@ -15,53 +18,59 @@ PASS_REGEX = r"^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$"
 user_regex_compiled = re.compile(USER_REGEX)
 pass_regex_compiled = re.compile(PASS_REGEX)
 
+db = database.Database(database_connection_string="sqlite:///user.db")
+
 
 class MainHandler(web.RequestHandler):
     """This wil process templates"""
 
     def get(self, arg1):
-        self.render("static.html", title= "Homepage")
-        #user = database.get_user_by_id(user_id)
-        #self.write("Het werkt: {}. Met user. {}".format(arg1, self.cookies.get("user_id")))
-        #er klopt hier iets niet!
+        # self.render("home.html", title="Homepage")
+        if arg1 == "":
+            arg1 = "home.html"
+        self.render(arg1, title="Homepage")
+        # user = database.get_user_by_id(user_id)
+        # self.write("Het werkt: {}. Met user. {}".format(arg1, self.cookies.get("user_id")))
+        # er klopt hier iets niet!
+
 
 class LoginHandler(web.RequestHandler):
     """Handles login form request with a body"""
+
+    # GEEN GET
 
     def post(self):
         user_name = self.get_body_argument("user-name")
         password = self.get_body_argument("password")
         # Bevatten user name en paswoord geen stoute dingen?
         if user_regex_compiled.match(user_name) is None:
-            self.redirect("/static/login.html?err=user%20not%20ok")
+            self.redirect("login.html?err=user%20not%20ok")  # Status 3XX
             return
+        """
         if pass_regex_compiled.match(password) is None:
-            self.redirect("/static/login.html?err=pass%20not%20ok")
+            self.redirect("login.html?err=pass%20not%20ok")
             return
+        """
 
-        hasher = hashlib.sha256()
-        hasher.update(password.encode("utf-8"))
-        # Add illegal char check?
-        hashed = hasher.hexdigest()
+        hashed = util.plaintext_to_hash(password)
 
-        try:
-            check_user = database.User.get_by_user_name(user_name)
-        except database.Not_Found_Exception:
-            self.redirect("/static/login.html?err=user%20not%20found")  # html file aangemaakt maar moet nog uitbreiden
+        session = db.get_session()
+
+        check_user = session.query(model.User).filter_by(user_name=user_name).first()  # type:model.User
+        if check_user is None:  # Not found
+            self.redirect("login.html?err=user%20not%20found")
             return
 
         # Check if pass hash matches
-        # Setting user id as cookie is not very safe. Make session object
+        # Setting user id as cookie is not very safe. Make state object
 
         if check_user.pass_hash == hashed:
+            # Pass is ok
             self.set_cookie("user_id", str(check_user.id))
-            self.redirect("/static/home.html")
+            self.redirect("home.html")
         else:
-            # self.redirect("/static/password-not-correct.html")
-            # self.redirect("/static/login.html?err=pass%20not%20correct") #snel een html file aangemaakt!
-
-            self.set_cookie("user_id", str(check_user.id))
-            self.redirect("/static/home.html")
+            # pass is not ok
+            self.redirect("login.html?err=pass%20not%20correct")
             return
 
 
@@ -85,7 +94,7 @@ class NewUserHandler(web.RequestHandler):
         new_user.password = hashed
         new_user.user_name = user_name
         new_user.save_to_db()
-        self.redirect("/static/login.html")
+        self.redirect("/templates/login.html")
 """
 
 
@@ -94,7 +103,7 @@ def make_tornado_app():
         (r"/login-action", LoginHandler),
         (r"/static/(.*)", web.StaticFileHandler, {"path": STATIC_FILE_DIR}),
         (r"/(.*)", MainHandler)
-    ])
+    ], template_path=TEMPLATE)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -8,31 +8,8 @@ db.create_schema(model.Base)
 
 session = db.get_session()
 
-# Add dennis
-dennis = model.User(first_name="Dennis",
-                    last_name="Van Eecke",
-                    user_name="dve",
-                    email="dennis.vaneecke@gmail.com",
-                    pass_hash=util.plaintext_to_hash("0mg-Pr@tput!"),
-                    role=blog_types.Role.ADMIN)
-
-session.add(dennis)
-
-session.commit()
 
 # Query dennis
-new_dennis = session.query(model.User).filter_by(user_name="dve").first()  # type: model.User
-if dennis is new_dennis:
-    print("Got the same instance")
-print("Role of dennis: " + new_dennis.role.name)
-
-# Change dennis
-dennis.email = "sinterklaas@telenet.be"
-session.commit()
-
-#Delete dennis
-session.query(model.User).filter_by(user_name="dve").delete(synchronize_session=False)
-session.commit()
-
-
+new_dennis = session.query(model.User).filter_by(user_name="sdfas").first()  # type: model.User
+print(new_dennis)
 session.close()
